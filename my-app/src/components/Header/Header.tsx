cf5fb4ed5309f0a1965c277b9b5c7d7ade5ff0f1
Anastasiia Bulgakova <nbulghakova@mail.ru>
2025-04-12 12:01:52 +0700

    fix

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -1,24 +1,22 @@
 import { Link, NavLink, Outlet } from "react-router-dom";
 import React from "react";
-import './header.css';
+import styles from './header.module.scss';
 const Header = () => {
     return ( 
       <>
-     
-       <header className="container">
-       <Link to='/' style={{
-                  textDecoration: 'none', 
-                  color: 'black'
-              }}>Realworld Blog</Link>
-      <div className="button-container">
-       <NavLink to='/sign-in' className='link'>Sign In</NavLink>
-      <NavLink to='/sign-up' className='link'>Sign Up</NavLink>
-      </div>
+       <header className={styles.containerNo}>
+        <Link to='/' style={{
+                    textDecoration: 'none', 
+                    color: 'black'
+                }}>Realworld Blog</Link>
+        <div className={styles.buttonContainer}>
+        <NavLink to='/sign-in' className={styles.link}>Sign In</NavLink>
+        <NavLink to='/sign-up' className={styles.link}>Sign Up</NavLink>
+        </div>
       </header> 
       <main>
       <Outlet/>
       </main>
-    
        </>
      );
 }
--- my-app/src/components/HeaderLoggedIn/HeaderLoggedIn.tsx
+++ my-app/src/components/HeaderLoggedIn/HeaderLoggedIn.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, NavLink, Outlet } from 'react-router-dom';
 import avatar from '../assets/images/Rectangle1.png';
-import './headerLoggedIn.css'
+import styles from './headerLoggedIn.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootSt } from '../../redux/store';
 import { logout } from '../../redux/authSlice';
@@ -18,22 +18,22 @@ const HeaderLoggedIn: React.FC = () => {
            return <Loading />
          }
     return ( 
-        <div className='main-header'>
+        <div className={styles.main}>
      
-        <header className="container">
+        <header className={styles.container}>
         <Link to='/' style={{
             textDecoration: 'none', 
             color: 'black'
         }}>Realworld Blog</Link>
-       <div className="button-container">
-        <NavLink to='new-article' className='link link-sm'>Create Article</NavLink>
-       <NavLink to='profile' className='link link-auth'>
-       <div className="author-cont">
-            <span className="name">{name}</span>
-            <img className="avatar" src={image || avatar} alt="" />
+       <div className={styles.buttonContainer}>
+        <NavLink to='new-article' className={`${styles.link} ${styles.linkSm}`}>Create Article</NavLink>
+       <NavLink to='profile' className={`${styles.link} ${styles.linkAuth}`}>
+       <div className={styles.authorCont}>
+            <span className={styles.name}>{name}</span>
+            <img className={styles.avatar} src={image || avatar} alt="" />
         </div>
         </NavLink>
-        <NavLink to='/' className='link logout' onClick={handleClick}>Log Out</NavLink>
+        <NavLink to='/' className={`${styles.link} ${styles.logout}`} onClick={handleClick}>Log Out</NavLink>
        </div>
        </header> 
        <main>
